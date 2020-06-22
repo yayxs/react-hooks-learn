6c56bac086639ad81edf148ab446ce04a953b037
yayxs <leaderywl@163.com>
2020-06-22 23:06:25 +0800

    feat(add usestate): add useState
    add useState

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,30 @@
-const IndexPage = () => <>1212</>;
+import { useState } from "react";
+import { Button } from "@material-ui/core";
 
-export default IndexPage;
+interface IInfo {
+  name: string;
+  age: number;
+}
+
+const Index = () => {
+  const [state, setstate] = useState(0);
+
+  const [name, setName] = useState<string>("yayxs");
+  const [info, setInfo] = useState<IInfo>({ name: "yayxs", age: 18 });
+
+  const handleClick = () => {
+    console.log(`按钮执行`);
+    console.log(name);
+    setstate(1);
+  };
+
+  return (
+    <>
+      展示一下State : {state}
+      <Button color="primary" onClick={handleClick}>
+        点我直接别墨迹
+      </Button>
+    </>
+  );
+};
+export default Index;
