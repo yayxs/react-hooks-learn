114b4608aa9603d6187d6c34978f9758dc948667
yayxs <leaderywl@163.com>
2020-06-25 00:13:56 +0800

    feat(add useeffect): add useEffect
    add useEffect

---
+++ pages/index copy 3.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+class App extends React.Component {
+  componentDidMount() {
+    // 生命周期钩子
+    console.log(1);
+  }
+  render() {
+    return <>qw</>;
+  }
+}
+
+export default App;
