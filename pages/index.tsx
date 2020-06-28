d4e0555a8176284e4c05031189882cf49e9fd730
yayxs <leaderywl@163.com>
2020-06-28 23:01:52 +0800

    feat(add useeffect): add useEffect
    add useEffect

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,49 +2,48 @@ import React, { ReactElement, useState, useEffect } from "react";
 import { Input, Container, Button, Box, Card, Switch } from "@material-ui/core";
 interface Props {}
 
+const MyInput = (): ReactElement => {
+  let [val, setVal] = useState(0);
+
+  const handle = (ev) => {
+    var event = ev || event;
+    if (event.keyCode == 13) {
+      let ipt = document.querySelector(".MuiInput-input");
+      console.log(`聚焦事件触发`);
+      ipt?.focus();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handle, false);
+
+    setInterval(() => {
+      val++;
+    }, 1000);
+
+    return () => {
+      document.removeEventListener("keydown", handle);
+    };
+  });
+  return (
+    <>
+      <Input
+        defaultValue={val}
+        inputProps={{ "aria-label": "description" }}
+        onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+          setVal(e.target.value);
+        }}
+      />
+    </>
+  );
+};
+
 export default function index({}: Props): ReactElement {
-  const [val, setVal] = useState(0);
-  // const [switchState, setSwitchState] = useState(true);
   const [isShowIpt, setIsShowIpt] = useState(true);
   const [state, setState] = React.useState({
     checkedA: true,
-    checkedB: true,
   });
-  // useEffect(() => {
-  //   effect
 
-  //   return () => {
-  //     cleanup
-  //   }
-  // })
-
-  useEffect(() => {
-    document.addEventListener(
-      "keydown",
-      (ev) => {
-        var event = ev || event;
-        if (event.keyCode == 13) {
-          let ipt = document.querySelector(".MuiInput-input");
-          console.log(ipt);
-          // ipt.focus();
-        }
-      },
-      false
-    );
-
-    // document.onkeydown = function (ev) {
-    //   var event = ev || event;
-    //   if (event.keyCode == 13) {
-    //     let ipt = document.querySelector(".MuiInput-input");
-    //     console.log(ipt);
-    //     // ipt.focus();
-    //   }
-    // };
-    return () => {
-      console.log(2);
-      document.removeEventListener("keydown", () => {});
-    };
-  });
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setIsShowIpt(!isShowIpt);
     setState({ ...state, [event.target.name]: event.target.checked });
@@ -53,42 +52,13 @@ export default function index({}: Props): ReactElement {
   return (
     <div>
       <Card>
-        {isShowIpt ? (
-          <Input
-            defaultValue={val}
-            inputProps={{ "aria-label": "description" }}
-            onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-              setVal(e.target.value);
-            }}
-          />
-        ) : (
-          <></>
-        )}
-
-        <Container maxWidth="sm"> {val}</Container>
-
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => {
-            setVal(val + 1);
-          }}
-        >
-          按钮
-        </Button>
+        {isShowIpt ? <MyInput /> : null}
 
         <Switch
           checked={state.checkedA}
           onChange={handleChange}
           name="checkedA"
           inputProps={{ "aria-label": "secondary checkbox" }}
-        />
-        <Switch
-          checked={state.checkedB}
-          onChange={handleChange}
-          color="primary"
-          name="checkedB"
-          inputProps={{ "aria-label": "primary checkbox" }}
         />
       </Card>
     </div>
