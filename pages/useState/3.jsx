46680ff109b23569c8e7a0ef5e5e18d0df821d63
yayxs <leaderywl@163.com>
2021-07-13 18:06:44 +0800

    ci(change npm to pnpm): change npm to pnpm

---
+++ pages/useState/3.jsx
@@ -0,0 +1,12 @@
+import React ,{useState } from "react";
+
+function Index(){
+
+  // console.log(this)
+  
+  const [name,setName] = useState('') // 函数组件内部存储state
+  const [pwd,setPwd] = useState('')
+
+  return null
+}
+export default Index
